1b614fd4ebb54ed5a226070c615fef952411d5fb
armaanepiic <armaanepiicfb17@gmail.com>
2025-09-23 19:40:01 +0600

    configure request logger

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import envConfig from "./config/envConfig.js";
-import configureRouters from './routers/index.js';
+import configureRouters from "./routers/index.js";
+import {logger} from "./middlewares/index.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: envConfig.ALLOWED_ORIGIN,
-}));
-const Logger = function (req, res, next) {
-  console.log(`[${new Date().toISOString()}]: ${req.method} | ${req.url}`);
-  next();
-}
-app.use(Logger);
+app.use(
+  cors({
+    origin: envConfig.ALLOWED_ORIGIN,
+  })
+);
+
+app.use(logger);
 
 configureRouters(app);
 
