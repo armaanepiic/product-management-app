602e18e90b005fc9f95bddcf14b173cd999f4b53
armaanepiic <armaanepiicfb17@gmail.com>
2025-09-23 20:42:36 +0600

    add controller layer

--- server/src/routers/product.js
+++ server/src/routers/product.js
@@ -1,19 +1,19 @@
 import {Router} from 'express'
 import { mockProducts } from '../mockdata.js';
 import { RouteNotImplementedError } from '../errors/index.js';
+import { getAllProducts } from '../controllers/product.js';
+import { productController } from '../controllers/index.js';
 
 const productRouter = Router();
 
-productRouter.post("/", (req, res) => {
-  throw new RouteNotImplementedError();
-})
+productRouter.post("/", (req, res) => productController.createProduct);
 
-productRouter.get('/', (req, res) => {
-  res.status(200).json(mockProducts);
-});
-productRouter.get("/:productId" , (req, res) => {
-  console.log(req.params);
-  const {productId} = req.params;
-  res.json({message: `Product ID : ${productId}`});
-});
+productRouter.get('/', (req, res) => productController.getAllProducts);
+
+productRouter.get("/:productId" , productController.getProductById);
+
+productRouter.put("/:productId" , productController.updateProduct);
+
+productRouter.delete("/:productID", productController.deleteProduct);
+
 export default productRouter;
