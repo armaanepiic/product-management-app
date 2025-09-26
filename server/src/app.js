27e91696ebf270724d9d6bcb2e13ac44b38616e0
armaanepiic <armaanepiicfb17@gmail.com>
2025-09-26 10:16:39 +0600

    -logger and routing added

--- server/src/app.js
+++ server/src/app.js
@@ -1,27 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import { mockProducts } from './mockdata.js';
+import express from "express";
+import cors from "cors";
+import { mockProducts } from "./mockdata.js";
 import envConfig from "./config/envConfig.js";
+import configureRouters from "./routers/index.js";
+import logger from "./middlewares/logger.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: envConfig.ALLOWED_ORIGIN,
-}));
+app.use(
+  cors({
+    origin: envConfig.ALLOWED_ORIGIN,
+  })
+);
 
-app.get("/status", (req, res) => {
-  res.json(
-    {
-      message : "Hello Application is RUNNING",
-    }
-  );
-});
+app.use(logger);
 
-app.get("/api/products" , (req, res) => {
-  res.status(200).json(mockProducts);
-})
-
+configureRouters(app);
 
 app.listen(envConfig.PORT, () => {
   console.log(`Example app listening on port ${envConfig.PORT}`);
