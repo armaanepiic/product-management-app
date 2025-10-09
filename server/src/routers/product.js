4baa4450e672feb973764179d50054fc8f73ba4d
armaanepiic <armaanepiicfb17@gmail.com>
2025-10-09 13:02:45 +0600

    add zod payload validation middleware

--- server/src/routers/product.js
+++ server/src/routers/product.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 
 import { productController } from "../controllers/index.js";
+import validatePayload from "../middlewares/validate.js";
+import { CreateProductSchema } from "../schemas/product.js";
 
 const productRouter = Router();
 
-productRouter.post("/", productController.createProduct);
+productRouter.post(
+  "/",
+  validatePayload(CreateProductSchema),
+  productController.createProduct
+);
 productRouter.get("/", productController.getAllProducts);
 productRouter.get("/:productId", productController.getProductById);
 productRouter.put("/:productId", productController.updateProduct);
